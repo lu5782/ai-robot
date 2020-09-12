9c10c4e94bc7f14478a99b6c83d519e55cc43a29
578228281 <578228281@qq.com>
2020-09-12 13:04:14 +0800

    删除冗余代码

--- ai-robot-api/src/main/java/com/cyp/robot/api/controller/IndexController.java
+++ ai-robot-api/src/main/java/com/cyp/robot/api/controller/IndexController.java
@@ -1,10 +1,8 @@
 package com.cyp.robot.api.controller;
 
-import com.cyp.robot.api.common.annotation.RequestLog;
 import lombok.extern.slf4j.Slf4j;;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -12,10 +10,8 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-        return "console";
+        return "clock";
     }
-
-
 
 
 }
