83171b2c6c1fceec9364ed130a900dad88c82fb5
578228281 <578228281@qq.com>
2021-02-28 21:18:58 +0800

    ..

--- ai-robot-api/src/main/java/com/cyp/robot/AiRobotApplication.java
+++ ai-robot-api/src/main/java/com/cyp/robot/AiRobotApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+@EnableScheduling   // 开启定时任务
 @EnableCaching
 @SpringBootApplication
 @EnableTransactionManagement
